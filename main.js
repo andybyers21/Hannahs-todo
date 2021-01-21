f10e0cb987980aca466baee503b1440566374667
andybyers21 <andy.byers21@gmail.com>
2021-01-21 15:47:29 +0000

    add clock

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-var checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
+// CLOCK
+
+function checkTime(i) {
+  if (i < 10) {
+    i = '0' + i;
+  }
+  return i;
+}
+
+function startTime() {
+  let today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  // add a zero in front of numbers<10
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML = h + ':' + m;
+  t = setTimeout(function () {
+    startTime();
+  }, 500);
+}
+startTime();
+
+// PERSISTANT CHECKBOXES
+
+let checkboxValues = JSON.parse(localStorage.getItem('checkboxValues')) || {},
   $checkboxes = $('#checkbox-container :checkbox');
 
 $checkboxes.on('change', function () {
